95ba5fb40c368555f519832c42a7b2e7fba2c80f
Miguel Mota <miguelmota2@gmail.com>
2015-07-16 00:38:42 -0700

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# flowright
+
+> Pebble Chrome tab switcher server.
+
+**To be used in conjuction with the [https://github.com/miguelmota/pebble-button-http-request](Pebble Button HTTP Request app).**
+
+# Install
+
+```bash
+git clone https://github.com/miguelmota/pebble-chrome-tab-switcher.git
+cd pebble-chrome-tab-switcher/
+npm install
+npm start
+```
+
+# Usage
+
+Pebble watch button actions:
+
+| Button  | Action  |
+|---|---|
+| Back  | Previous page in history  |
+| Select  | Next page in history  |
+| Up  | Previous tab  |
+| Down  | Next tab  |
+
+# License
+
+MIT
---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var controller = require('./lib/controller');
+
+var BUTTONS = {
+  'BACK': 'BACK',
+  'UP': 'UP',
+  'SELECT': 'SELECT',
+  'DOWN': 'DOWN'
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+function handleRequest(req, res) {
+  var body = req.body || {};
+  var button = req.body.button;
+
+  console.log(body);
+
+  if (button && BUTTONS[button]) {
+    if (button === BUTTONS.BACK) {
+      controller.sendAction(controller.ACTIONS.BACK);
+    } else if (button === BUTTONS.SELECT) {
+      controller.sendAction(controller.ACTIONS.FORWARD);
+    } else if (button === BUTTONS.UP) {
+      controller.sendAction(controller.ACTIONS.PREV_TAB);
+    } else if (button === BUTTONS.DOWN) {
+      controller.sendAction(controller.ACTIONS.NEXT_TAB);
+    }
+
+    return res.sendStatus(200);
+  }
+
+  res.sendStatus(400);
+}
+
+app.post('/data', handleRequest);
+
+var port = process.env.PORT || 5468;
+
+var server = app.listen(port, function () {
+  var port = server.address().port;
+
+  console.log('Listening at http://localhost:%s', port);
+});
